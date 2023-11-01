749e90cc7aaa445abd121e761822d900c5c55d07
Gopal1252 <guptagopal252@gmail.com>
2023-11-01 22:45:45 +0530

    initial commit

---
+++ facebook_login_clone/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      colors:{
+        fbBlue : "#2074f4",
+        fbGreen : "#42b829",
+        fbdarkGreen : "#38a424"
+      }
+    },
+  },
+  plugins: [],
+}
+
